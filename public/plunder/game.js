03743d7e1c4a772ac26e29c587e462699d005293
Andrew Brossia <abrossia@gmail.com>
2024-12-26 09:32:09 -0500

    Make more concise

--- public/plunder/game.js
+++ public/plunder/game.js
@@ -4,8 +4,8 @@ function random_element_from(array) {
 }
 
 function spin() {
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-  const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"];
+  const numbers = Array.from({ length: 19 }, (v, k) => k + 1);
+  const letters = [...'ABCDEFGHIJKL'];
   const number = random_element_from(numbers);
   const letter = random_element_from(letters);
   document.getElementById("spin-output").innerHTML = letter + " / " + number;

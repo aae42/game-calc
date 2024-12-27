b98b6fb34b441d9481bea00db730e4e21d891b4f
Andrew Brossia <abrossia@gmail.com>
2024-12-26 21:52:58 -0500

    Adjust test name
    way better description
    
    also adjust deploy to rely on test

--- test/plunder.test.ts
+++ test/plunder.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "bun:test";
 import { getRandomCoords } from "../public/plunder/game";
 
-test.each(Array(1000).fill(null))("getRandomCoords gets good coords %#", () => {
+test.each(Array(1000).fill(null))("getRandomCoords doesn't roll a 19 %#", () => {
     expect(getRandomCoords().number).not.toBe(19);
 });
